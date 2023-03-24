a53139a7a6fbec8fc1e70ea0c6084077949efd20
dyslexicsteak <49301588+dyslexicsteak@users.noreply.github.com>
2023-03-24 16:53:18 +0300

    Add constructor and noexcept
    Remove #pragma once, add constructor, add noexcept

--- ordered_reference.hpp
+++ ordered_reference.hpp
@@ -1,5 +1,3 @@
-#pragma once
-
 #ifndef ORDERED_REFERENCE_HPP
 #define ORDERED_REFERENCE_HPP
 
@@ -7,15 +5,19 @@
 #include <set>
 
 namespace ordered_reference {
-using std::reference_wrapper;
-using std::strong_ordering;
-using std::three_way_comparable;
+using std::reference_wrapper, std::strong_ordering, std::three_way_comparable;
 
 template <typename T, three_way_comparable U> struct ordered_reference_t {
   reference_wrapper<T> reference;
 
-  friend constexpr strong_ordering operator<=>(const ordered_reference_t &lhs,
-                                               const ordered_reference_t &rhs) {
+  ordered_reference_t<T, U>(const T &referenced, const U &weight) noexcept {
+    this->reference = referenced;
+    this->_weight = weight;
+  }
+
+  friend constexpr strong_ordering
+  operator<=>(const ordered_reference_t &lhs,
+              const ordered_reference_t &rhs) noexcept {
     return lhs._weight <=> rhs._weight;
   }
 
